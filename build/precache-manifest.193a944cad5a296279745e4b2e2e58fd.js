16999de113aa35d92596c0d1bdee69a4e3927e33
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2020-06-14 21:43:37 -0300

    Navbar Fix

---
+++ build/precache-manifest.193a944cad5a296279745e4b2e2e58fd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40a235e302e7bbcd390a6405ee38564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0ab1aeb855c17665e3d",
+    "url": "/static/css/2.97d3883c.chunk.css"
+  },
+  {
+    "revision": "b17bd7336811f9b6cb99",
+    "url": "/static/css/main.97fcba78.chunk.css"
+  },
+  {
+    "revision": "a0ab1aeb855c17665e3d",
+    "url": "/static/js/2.a5530f46.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.a5530f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17bd7336811f9b6cb99",
+    "url": "/static/js/main.4cd5f8eb.chunk.js"
+  },
+  {
+    "revision": "4dbc644edc03f789fd26",
+    "url": "/static/js/runtime-main.5dcfc514.js"
+  },
+  {
+    "revision": "0c546f84539a6b25a5d6b0e94413c0b5",
+    "url": "/static/media/Save.0c546f84.svg"
+  },
+  {
+    "revision": "309d8adc6c7cd6249fb3048712ac1ce2",
+    "url": "/static/media/estimator.309d8adc.svg"
+  },
+  {
+    "revision": "d7314245a949310c32475f1ddf5a2c62",
+    "url": "/static/media/facts.d7314245.svg"
+  },
+  {
+    "revision": "801207af7eaabf54d0af0f986e83cbe4",
+    "url": "/static/media/pic.801207af.jpg"
+  }
+]);
